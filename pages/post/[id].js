4432c351329c1ac04c74684da88ce67fcf6120bf
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-07-08 10:23:36 -0500

    Comments show up under correct post

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -3,31 +3,26 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { getCommentsByPostId } from '../../utils/data/commentData';
-import { getSinglePost } from '../../utils/data/postData';
 import CommentCard from '../../components/comment/CommentCard';
 
 export default function PostComment() {
   const router = useRouter();
-  const postIdRouter = router.query;
-  const [postId, setPostId] = useState();
   const [comments, setComments] = useState([]);
   const { id } = router.query;
 
   const getAllComments = () => {
-    getCommentsByPostId(postId).then((data) => setComments(data));
+    getCommentsByPostId(id).then((data) => setComments(data));
   };
 
   useEffect(() => {
-    console.warn(postIdRouter);
+    console.warn(id);
     getAllComments();
-    getSinglePost(postIdRouter.id).then((data) => setPostId(data.id));
-  }, [postIdRouter]);
+  }, []);
 
   return (
     <div>
       <h2>Post Comment</h2>
       <Button onClick={() => {
-        // Navigate to the "/comments/new" page with the "postId" query parameter set to "id"
         router.push({
           pathname: '/comments/new',
           query: {
@@ -40,7 +35,14 @@ export default function PostComment() {
       </Button>
       {comments.map((comment) => (
         <section key={`comment--${comment.id}`} className="comment">
-          <CommentCard content={comment.content} postId={comment.post_id} authorId={comment.author_id} createdOn={comment.created_on} id={comment.id} onUpdate={getAllComments} />
+          <CommentCard
+            content={comment.content}
+            postId={comment.post_id}
+            authorId={comment.author_id}
+            createdOn={comment.created_on}
+            id={comment.id}
+            onUpdate={getAllComments}
+          />
         </section>
       ))}
     </div>
